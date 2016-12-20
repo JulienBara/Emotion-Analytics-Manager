ef4cda3640b52bc51721332b1dc97652b0a48d6d
JulienBara <bara.julien@yahoo.fr>
2016-12-20 12:50:55 +0100

    clean

--- emo.py
+++ emo.py
@@ -1,21 +1,19 @@
-#coding=utf-8
+# coding=utf-8
 
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-from collections import deque
+from telegram.ext import Updater, CommandHandler
 import requests
 import logging
 
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-version = '1.0'
-
 # Read keys
 telegramKey = open('./key').read().splitlines()[0]
 yandexKey = open('./yandexKey').read().splitlines()[0]
 watsonKey = open('./apikey').read().splitlines()[0]
 
-# Basic commands 
+
+# Basic commands
 def start(bot, update):
     chat_id = update.message.chat_id
     message = 'Bonjour, je suis Emotion Analytics Manager'
@@ -25,20 +23,20 @@ def start(bot, update):
 def emo(bot, update, args):
     chat_id = update.message.chat_id
     mot = ' '.join(args)
-    message = analyseEmotion(mot)
+    message = analyse_emotion(mot)
     bot.sendMessage(chat_id, text=message)
 
 
 # Functions
-def analyseEmotion(message: str) -> str:
+def analyse_emotion(message: str) -> str:
     url = "https://translate.yandex.net/api/v1.5/tr.json/translate"
-    query = { 'key': yandexKey,
-              'lang': 'en',
-              'text': message}
+    query = {'key': yandexKey,
+             'lang': 'en',
+             'text': message}
 
     r = requests.post(url, data=query)
 
-    if(r.status_code == 200):
+    if r.status_code == 200:
         print(r.status_code)
         print(r.text)
         print(r.json())
@@ -47,50 +45,52 @@ def analyseEmotion(message: str) -> str:
         print(translation)
 
         url = "http://gateway-a.watsonplatform.net/calls/text/TextGetEmotion"
-        query = { 'apikey': watsonKey,
-                'text': translation,
-                'outputMode': 'json'}    
-        
+        query = {'apikey': watsonKey,
+                 'text': translation,
+                 'outputMode': 'json'}
+
         r = requests.post(url, data=query)
         print(r.status_code)
         print(r.text)
         print(r.json())
 
-        docEmotions = r.json()['docEmotions']
+        doc_emotions = r.json()['doc_emotions']
 
         # new output
         # i = 1    
         # anger = ""
-        # while i < float(docEmotions['anger']) * 10:
+        # while i < float(doc_emotions['anger']) * 10:
         #     anger += "*"
-        
+
         # i = 1
         # disgust = ""
-        # while i < float(docEmotions['disgust']) * 10:
+        # while i < float(doc_emotions['disgust']) * 10:
         #     disgust += "*"
 
         # i = 1
         # fear = ""
-        # while i < float(docEmotions['fear']) * 10:
+        # while i < float(doc_emotions['fear']) * 10:
         #     fear += "*"
 
         # i = 1
         # joy = ""
-        # while i < float(docEmotions['joy']) * 10:
+        # while i < float(doc_emotions['joy']) * 10:
         #     joy += "*"
 
         # i = 1
         # sadness = ""
-        # while i < float(docEmotions['sadness']) * 10:
+        # while i < float(doc_emotions['sadness']) * 10:
         #     sadness += "*"
 
         # ret = "Colère = " + anger + "\n" + "Dégout = " + disgust + "\n" + "Peur = " + fear + "\n" + "Joie = " + joy + "\n" + "Tristesse = " + sadness + "\n"
 
         # old output
-        ret = "Colère = " + docEmotions['anger'] + "\n" + "Dégout = " + docEmotions['disgust'] + "\n" + "Peur = " + docEmotions['fear'] + "\n" + "Joie = " + docEmotions['joy'] + "\n" + "Tristesse = " + docEmotions['sadness'] + "\n"
+        ret = "Colère = " + doc_emotions['anger'] + "\n" + "Dégout = " + doc_emotions['disgust'] + "\n" + "Peur = " + \
+              doc_emotions['fear'] + "\n" + "Joie = " + doc_emotions['joy'] + "\n" + "Tristesse = " + doc_emotions[
+                  'sadness'] + "\n"
 
     else:
-        ret = "Je suis en PLS. Pose cette poule et reviens plus tard. (Status code = " + str(r.status_code) +")"
+        ret = "Je suis en PLS. Pose cette poule et reviens plus tard. (Status code = " + str(r.status_code) + ")"
     return ret
 
 
